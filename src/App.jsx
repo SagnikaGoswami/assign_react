1f03c63ef360063a5078d1c50f669422d3bf4700
Sagnika Goswami <sagnikagoswami@gmail.com>
2026-04-01 22:24:46 +0530

    Initial files loaded

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+
+const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    }
+  ])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
