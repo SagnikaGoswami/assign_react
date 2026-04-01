13443f8065559e2ec2cab7efc3ab80265f1e9780
Sagnika Goswami <sagnikagoswami@gmail.com>
2026-04-01 22:29:02 +0530

    Font Style Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
-  return (
-    <div>
-        <nav className="bg-purple-400 flex justify-evenly items-center h-16">
-            <div className="font-['Erbil'] font-normal text-[28px] leading-[46px] tracking-normal text-center">
-                Elementum
-            </div>
-        </nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="bg-purple-400 flex justify-evenly items-center h-16">
+                <div className="font-['Gerbil'] font-normal text-[28px] leading-[46px] tracking-normal text-center">
+                    Elementum
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
