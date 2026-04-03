09a77f28342564bdd2f155f58b965623c388c2c2
Sagnika Goswami <sagnikagoswami@gmail.com>
2026-04-03 19:09:49 +0530

    Hero section UI improved

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -106,54 +106,54 @@ const HeroSection = () => {
             type: "img",
             src: "/images/img1.png",
             alt: "img1",
-            position: "top-1/2 right-[80%] md:top-[25%] md:left-[10%] lg:top-1/2 lg:left-0 -translate-y-1/2"
+            position: "top-1/2 right-[80%] md:top-1/2 md:left-0  lg:top-1/2 lg:left-0 -translate-y-1/2"
         },
         {
             type: "svg",
             alt: "circle",
-            position: "top-[30%] left-[5%] md:top-[35%] md:left-[25%] lg:top-1/3 lg:left-[10%] -translate-y-1/2"
+            position: "top-[30%] left-[5%] md:top-[35%] md:left-[10%] lg:top-1/3 lg:left-[10%] -translate-y-1/2"
         },
         {
             type: "img",
             src: "/images/img3.png",
             alt: "img3",
-            position: "top-[5%] left-[40%] z-0 md:top-[20%] md:left-[35%] lg:top-[12%] lg:left-[26%] lg:z-10"
+            position: "top-[5%] left-[40%] z-0 md:top-[5%] md:left-[30%] lg:top-[12%] lg:left-[26%] lg:z-10"
         },
         {
             type: "svg",
             alt: "circle",
-            position: "top-[25%] left-[38%] z-10 md:top-[45%] md:left-[40%] lg:top-[40%] lg:left-[33%] lg:z-0"
+            position: "top-[25%] left-[38%] z-10 md:top-[40%] md:left-[35%] lg:top-[40%] lg:left-[33%] lg:z-0"
         },
         {
             type: "svg",
             alt: "circle",
-            position: "top-[65%] left-[53%] z-10 md:top-[55%] md:left-[45%] lg:top-[10%] lg:left-[48%] lg:z-0"
+            position: "top-[65%] left-[53%] z-10 md:top-[8%] md:left-[55%] md:z-0 lg:top-[10%] lg:left-[48%] lg:z-0"
         },
         {
             type: "svg",
             alt: "circle",
-            position: "top-[15%] left-[70%] md:top-[65%] md:left-[55%] lg:top-0 lg:left-[75%] z-10"
+            position: "top-[15%] left-[70%] md:top-[8%] md:left-[80%] md:z-10 lg:top-0 lg:left-[75%] z-10"
         },
         {
             type: "img",
             src: "/images/img8.png",
             alt: "img8",
-            position: "top-[35%] left-[80%] md:top-[30%] md:left-[70%] lg:top-[33%] lg:left-[88%] z-0"
+            position: "top-[35%] left-[80%] md:top-[30%] md:left-[85%] md:z-0 lg:top-[33%] lg:left-[88%] z-0"
         },
         {
             type: "img",
             src: "/images/img6.png",
             alt: "img6",
-            position: "top-[60%] left-[30%] z-0 md:top-[75%] md:left-[50%] lg:top-[30%] lg:left-[56%] lg:z-10"
+            position: "top-[60%] left-[30%] z-0 md:top-[35%] md:left-[60%] md:z-10 lg:top-[30%] lg:left-[56%] lg:z-10"
         }
     ];
 
 
 
     return (
-        <div className="mb-20 md:mb-42">
+        <div className="mb-20 md:mb-42 ">
 
-            <div className="w-full px-4 mx-auto mt-20">
+            <div className="w-full px-4 mt-20 relative">
                 {/* head section*/}
                 <div className='w-[90%] mx-auto'>
                     <p className="font-[400] text-[30px] md:text-[60px] lg:text-[100px] leading-[48px] md:leading-[75px] lg:leading-[126px] font-['Gerbil'] tracking-normal text-center">
@@ -189,9 +189,13 @@ const HeroSection = () => {
                             </span>
                         </span>
 
+                        <svg className="absolute right-[20px] bottom-[85%] w-[50px] h-[60px] md:right-[20px]  md:bottom-[72%] lg:right-[12px] lg:bottom-[70%] lg:w-[117px] lg:h-[146px] md:w-[85px] md:h-[106px]"
+                            width="117" height="146" viewBox="0 0 117 146" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M77.8783 -4.93067e-06C86.7352 5.11355 94.4983 11.9216 100.724 20.0353C106.95 28.149 111.517 37.4095 114.164 47.2882C116.811 57.1668 117.486 67.4701 116.151 77.6097C114.816 87.7494 111.497 97.5268 106.384 106.384C101.27 115.241 94.4622 123.004 86.3484 129.23C78.2347 135.455 68.9742 140.022 59.0955 142.669C49.2169 145.316 38.9136 145.992 28.774 144.657C18.6344 143.322 8.85694 140.003 -7.20974e-06 134.889L38.9391 67.4446L77.8783 -4.93067e-06Z" fill="#934CEC" />
+                        </svg>
+
+
                         the
-
-
                         <span className="relative inline-block mx-4 lg:mx-10">
                             <span className="relative z-10 px-3">status</span>
                             <svg
@@ -204,20 +208,30 @@ const HeroSection = () => {
                                 <rect width="456" height="109" rx="54.5" fill="#D7EEDD" />
                             </svg>
                         </span>
-
-
-
                         Quo with
                     </p>
-                    <p className="font-[400] text-[16px] md:text-[20px] lg:text-[24px] leading-[24px] md:leading-[30px] lg:leading-[36px] font-['Satoshi-Regular'] tracking-normal text-center mt-5 w-[90%] md:w-[80%] lg:w-[70%] mx-auto">
+                    <p className="font-[400] text-[16px] md:text-[20px] lg:text-[24px] leading-[24px] md:leading-[30px] lg:leading-[36px] font-['Satoshi-Regular'] tracking-normal text-center mt-5 w-[90%] md:w-[80%] lg:w-[70%] lg:mt-25 mx-auto">
                         We are a team of strategists, designers communicators, researchers. Together,
                         we belive that progress only happens when you refuse to play things safe.
                     </p>
                 </div>
 
+                {/* vectors */}
+                <svg className="absolute left-0 top-[45%] lg:left-0 lg:top-[40%] lg:w-[73px] lg:h-[366px] md:h-[250px] h-[150px]"
+                viewBox="0 0 73 366" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M-0.125563 3.81958C32.8912 14.0867 91.0122 45.1979 59.3623 87.5062C19.8 140.391 -8.5 121.794 -8.5 157.245C-8.5 192.696 44.6348 192.696 44.6348 268.246C44.6348 328.686 2.95478 362.393 -8.5 361.812" stroke="#FF7171" stroke-width="8" />
+                </svg>
+
+                <svg className="absolute left-[2%] top-[45%] lg:top-[40%] lg:w-[84px] lg:h-[355px] md:h-[250px] h-[150px]"
+                    viewBox="0 0 84 355" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.99996 3.81927C36.9367 14.0704 103.271 45.1331 71.6979 87.3753C32.2314 140.178 4 121.61 4 157.005C4 192.4 57.006 192.4 57.006 267.833C57.006 328.179 15.427 351.389 4 350.809" stroke="#0E0E0E" stroke-width="8" />
+                </svg>
+
+
+
 
                 {/* image section */}
-                <div className='w-[90%] mx-auto md:mt-25 relative h-[300px] md:h-[400px]'>
+                <div className='absolute w-[90%] mx-auto md:mt-25 lg:mt-25 relative h-[300px] md:h-[400px] '>
                     {images.map((image, index) => (
                         image.type === "img" ? (
                             <img
