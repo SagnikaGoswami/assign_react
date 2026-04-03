382fee08190a65291512e8b527b086319925fdb6
Sagnika Goswami <sagnikagoswami@gmail.com>
2026-04-03 21:31:42 +0530

    Features UI imporved

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,15 +3,42 @@ import { BsArrowRight } from "react-icons/bs";
 const Features = () => {
     return (
         <div className="mb-40 md:mb-62">
-            <div className='flex flex-col justify-between w-[88%] mx-auto min-h-screen lg:min-h-[650px] '>
+            <div className='flex flex-col relative justify-between w-[88%] mx-auto min-h-screen lg:min-h-[650px] '>
                 {/* first */}
-                <div className='flex flex-col md:flex-row md:justify-end w-full mb-20 md:mb-30'>
+                <div className='flex flex-col relative md:flex-row md:justify-end w-full mb-20 md:mb-30 overflow-hidden'>
                     {/* left */}
-                    <div className=' w-full md:w-[70%]  flex justify-start items-center'>
+                    <div className=' w-full md:w-[70%]  flex justify-start items-center '>
                         <div className='w-full flex flex-col justify-center gap-y-3 md:gap-y-6'>
                             <p className="font-[400] text-[30px] md:text-[60px] lg:text-[56px] leading-[38px] md:leading-[75px] lg:leading-[76px] font-['Gerbil'] tracking-normal text-left">
-                                Tomorrow should
-                                be better than today
+
+                                <span className="relative inline-block pb-2 md:pb-7 md:mr-10 md:ml-10 mr-4 ml-4">
+                                    Tomorrow
+                                    <svg
+                                        className="absolute left-0 bottom-0.1 w-full"
+                                        viewBox="0 0 595 56"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M72.1335 3H538.634L0.133545 27H594.134L293.134 53" stroke="#FFC250" strokeWidth="6" />
+                                    </svg>
+                                </span>
+
+                                should
+                                be better than
+
+                                <span className="relative inline-block mx-1 lg:mx-10">
+                                    <span className="relative z-10 px-3">today</span>
+                                    <svg
+                                        className="absolute left-[20px] md:left-[30px] lg:left-[25px] top-1/2 lg:top-[45px] -translate-y-1/2 -translate-x-[15%] w-full  lg:w-[4.2em] h-[1.2em] rounded-full z-0"
+                                        viewBox="0 0 456 109"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        preserveAspectRatio="none"
+                                    >
+                                        <rect width="456" height="109" rx="54.5" fill="#D7EEDD" />
+                                    </svg>
+                                </span>
+
                             </p>
 
                             <p className="  font-[400] text-[16px] md:text-[20px] lg:text-[24px] leading-[24px] md:leading-[30px] lg:leading-[36px] font-['Satoshi-Regular'] tracking-normal text-left w-full">
@@ -21,7 +48,13 @@ const Features = () => {
                             </p>
 
                             <p className=" font-medium text-[12px] md:text-[16px] lg:text-[20px] leading-[12px] md:leading-[18px] lg:leading-[24px] font-['Satoshi-Regular'] tracking-normal text-left w-full mt-3">
-                                {`Read more`} <BsArrowRight className="inline-block ml-2 cursor-pointer" />
+                                {`Read more`}
+                                <svg className="inline-block ml-2 cursor-pointer w-[100px] h-[10px] lg:w-[145px] lg:h-[8px]"
+                                    viewBox="0 0 145 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M144.354 4.03556C144.549 3.8403 144.549 3.52372 144.354 3.32845L141.172 0.146473C140.976 -0.0487893 140.66 -0.0487893 140.464 0.146473C140.269 0.341735 140.269 0.658318 140.464 0.85358L143.293 3.68201L140.464 6.51043C140.269 6.7057 140.269 7.02228 140.464 7.21754C140.66 7.4128 140.976 7.4128 141.172 7.21754L144.354 4.03556ZM0 3.68201V4.18201H144V3.68201V3.18201H0V3.68201Z" fill="black" />
+                                </svg>
+
+                                {/* <BsArrowRight className="inline-block ml-2 cursor-pointer" /> */}
                             </p>
                         </div>
                     </div>
@@ -29,25 +62,113 @@ const Features = () => {
                     {/* right */}
                     <div className='flex justify-center md:justify-end items-center w-full  md:w-[50%] mt-10 md:mt-0'>
                         <img src="/images/right.png" alt=""
-                        className="h-60 w-60 md:h-80 md:w-80 lg:h-95 lg:w-95 rounded-full object-cover border-4 border-white"
+                            className="h-60 w-60 md:h-80 md:w-80 lg:h-95 lg:w-95 rounded-full object-cover border-4 border-white z-10"
                         />
+
+                        <svg className="w-[100px] h-[120px] left-[65%] bottom-[25%] md:w-[150px] md:h-[170px] lg:w-[274px] lg:h-[267px] md:bottom-[55%] md:left-[85%] z-0 absolute lg:bottom-[25%] lg:left-[90%]"
+                            viewBox="0 0 233 227" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M45.3254 -2.02656e-06L232.277 40.5018L186.952 226.343L4.78625e-05 185.841L45.3254 -2.02656e-06Z" fill="#FF7171" />
+                        </svg>
+
                     </div>
                 </div>
 
+{/* width="1483" height="654"  */}
+                <svg 
+
+                className="absolute inset-x-0 lg:top-[23%] lg:left-[5%] lg:w-[88.7vw] lg:h-[650px]"
+                
+                viewBox="0 0 1483 654" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_d_2_9)">
+                        <path d="M1488.65 134.467C1446.64 22.3687 1055.93 -89.3365 963 120.86C835.832 408.503 777.269 398.935 693.689 397.279C610.11 395.623 306.809 225.312 181.153 337.967C41.357 463.299 144.653 654.467 12.6534 618.727" stroke="#FF6D6D" stroke-width="5" />
+                    </g>
+                    <defs>
+                        <filter id="filter0_d_2_9" x="0" y="0" width="1502.99" height="653.604" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                            <feOffset dy="16" />
+                            <feGaussianBlur stdDeviation="6" />
+                            <feComposite in2="hardAlpha" operator="out" />
+                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_9" />
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_9" result="shape" />
+                        </filter>
+                    </defs>
+                </svg>
+
+
                 {/* second */}
-                <div className='flex flex-col mt-10 md:flex-row md:justify-end w-full'>
+                <div className='flex flex-col relative mt-5 lg:mt-10 md:flex-row md:justify-end w-full'>
                     {/* left */}
-                    <div className='flex justify-center md:justify-start items-center w-full md:w-[50%] mb-10 md:mb-0'>
-                        <img src="/images/left.png" alt=""
-                        className="h-60 w-60 md:h-80 md:w-80 lg:h-95 lg:w-95 rounded-full object-cover border-4 border-white"
+                    <div className="flex justify-center md:justify-start items-center w-full md:w-[50%] mb-10 md:mb-0 relative">
+
+                        <svg
+                            className="absolute w-[100px] h-[90px] left-[5%] top-0 md:w-[170px] md:h-[120px] lg:w-[238px] lg:h-[201px] lg:left-0 lg:bottom-[65%] md:right-[65%] md:bottom-[60%] z-0"
+                            viewBox="0 0 238 201"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M118.645 0L237.291 200.25H0L118.645 0Z" fill="#FF7171" />
+                        </svg>
+
+                        <img
+                            src="/images/left.png"
+                            alt=""
+                            className="h-60 w-60 md:h-80 md:w-80 lg:h-[95] lg:w-[95] rounded-full object-cover border-4 border-white z-10"
                         />
+
+                        <svg
+                            className="absolute w-[100px] h-[90px]  md:w-[170px] md:h-[120px] lg:w-[238px] lg:h-[201px] top-[68%] left-[60%] md:left-[40%] md:top-[60%] lg:left-[30%] lg:top-[45%] z-20"
+                            viewBox="0 0 238 201"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M118.646 0L237.291 200.25H3.05176e-05L118.646 0Z" fill="#FF7171" />
+                        </svg>
                     </div>
+
 
                     {/* right */}
                     <div className=' w-full md:w-[70%]  flex justify-start md:justify-end items-center relative'>
                         <div className=' w-full  flex flex-col justify-center gap-y-3 md:gap-y-6'>
                             <p className="font-[400] text-[30px] md:text-[60px] lg:text-[56px] leading-[38px] md:leading-[75px] lg:leading-[76px] font-['Gerbil'] tracking-normal text-left">
-                                See how we can help you progress
+
+                                <span className="relative inline-block mx-2 lg:mx-10">
+                                    <span className="relative z-10 px-3">See</span>
+                                    <svg
+                                        className="absolute left-[15px] md:left-[30px] lg:left-[25px] top-1/2 lg:top-[45px] -translate-y-1/2 -translate-x-[15%] w-full  lg:w-[3.2em] h-[1.2em] rounded-full z-0"
+                                        viewBox="0 0 456 109"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        preserveAspectRatio="none"
+                                    >
+                                        <rect width="456" height="109" rx="54.5" fill="#D7EEDD" />
+                                    </svg>
+                                </span>
+
+
+                                how we can help
+
+                                <span className="relative inline-block pb-2 md:pb-7 ml-1 md:mr-4 md:ml-8">
+                                    you
+                                </span>
+
+
+
+                                <span className="relative inline-block pb-2 md:pb-7 ml-1 md:mr-4 md:ml-8">
+                                    progress
+
+                                    <svg
+                                        className="absolute left-0 bottom-0.1 w-full"
+                                        viewBox="0 0 595 56"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M72.1335 3H538.634L0.133545 27H594.134L293.134 53" stroke="#FFC250" strokeWidth="6" />
+                                    </svg>
+                                </span>
+
+
                             </p>
 
                             <p className="  font-[400] text-[16px] md:text-[20px] lg:text-[24px] leading-[24px] md:leading-[30px] lg:leading-[36px] font-['Satoshi-Regular'] tracking-normal text-left">
@@ -57,7 +178,14 @@ const Features = () => {
                             </p>
 
                             <p className="  font-medium text-[12px] md:text-[16px] lg:text-[20px] leading-[12px] md:leading-[18px] lg:leading-[24px] font-['Satoshi-Regular'] tracking-normal text-left w-full mt-3">
-                                {`Read more`} <BsArrowRight className="inline-block ml-2 cursor-pointer" />
+                                {`Read more`}
+
+                                <svg className="inline-block ml-2 cursor-pointer w-[100px] h-[10px] lg:w-[145px] lg:h-[8px]"
+                                    viewBox="0 0 145 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M144.354 4.03556C144.549 3.8403 144.549 3.52372 144.354 3.32845L141.172 0.146473C140.976 -0.0487893 140.66 -0.0487893 140.464 0.146473C140.269 0.341735 140.269 0.658318 140.464 0.85358L143.293 3.68201L140.464 6.51043C140.269 6.7057 140.269 7.02228 140.464 7.21754C140.66 7.4128 140.976 7.4128 141.172 7.21754L144.354 4.03556ZM0 3.68201V4.18201H144V3.68201V3.18201H0V3.68201Z" fill="black" />
+                                </svg>
+
+                                {/* <BsArrowRight className="inline-block ml-2 cursor-pointer" /> */}
                             </p>
                         </div>
                     </div>
